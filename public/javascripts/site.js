a78e2a55a17bd5babe6454cc0035e055c7252112
raingggg <raingggg@protonmail.com>
2021-11-10 19:01:18 +0800

    tab show

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -8,4 +8,21 @@ $(document).ready(function () {
     $.cookie('language', 'de');
     window.location.reload();
   });
+
+  $('.nav-tab').click(function () {
+    const index = $(this).data("tabp-index");
+    const page = $(this).closest('.page-wrapper');
+    if (page && index >= 0) {
+      page.find('.tab-panel').each(function () {
+        if ($(this).data("panel-index") == index) {
+          $(this).removeClass('d-none');
+        } else {
+          $(this).addClass('d-none');
+        }
+      });
+    }
+  });
+
+
 });
+
