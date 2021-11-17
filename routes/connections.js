3590478439e9e78b553007215279517c46902eaa
raingggg <raingggg@nomail.com>
2021-11-17 11:43:42 +0800

    connect sort chainname

--- routes/connections.js
+++ routes/connections.js
@@ -4,7 +4,11 @@ const router = express.Router();
 
 const { Connection } = require('../models');
 router.get('/', async (req, res, next) => {
-  const data = await Connection.findAll();
+  const data = await Connection.findAll({
+    order: [
+      ['blockchain', 'ASC'],
+    ]
+  });
   res.render('index', { title: req.__('Welcome to Express'), data, pageName: 'connections' });
 });
 
