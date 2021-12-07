833efff932d4eca4593b5000d40f4c1e20081869
raingggg <raingggg@nomail.com>
2021-12-07 21:57:44 +0800

    add more blockchain explorer link

--- utils/chainConfigs.js
+++ utils/chainConfigs.js
@@ -1,4 +1,6 @@
 const chainConfigs = {
+  default: { exp: 'https://alltheblocks.net/chia/address/' },
+
   cactus: { exp: 'https://alltheblocks.net/cactus/address/' },
   covid: { exp: 'https://alltheblocks.net/covid/address/' },
   cryptodoge: { exp: 'https://alltheblocks.net/cryptodoge/address/' },
@@ -9,12 +11,26 @@ const chainConfigs = {
   pipscoin: { exp: 'https://alltheblocks.net/pipscoin/address/' },
   shibgreen: { exp: 'https://alltheblocks.net/shibgreen/address/' },
   silicoin: { exp: 'https://alltheblocks.net/silicoin/address/' },
+
   skynet: { exp: 'https://alltheblocks.net/skynet/address/' },
   staicoin: { exp: 'https://alltheblocks.net/stai/address/' },
   stor: { exp: 'https://alltheblocks.net/stor/address/' },
   tranzact: { exp: 'https://alltheblocks.net/tranzact/address/' },
   venidium: { exp: 'https://alltheblocks.net/venidium/address/' },
-  default: { exp: 'https://alltheblocks.net/chia/address/' },
+  btcgreen: { exp: 'https://alltheblocks.net/btcgreen/address/' },
+  hddcoin: { exp: 'https://alltheblocks.net/hddcoin/address/' },
+  maize: { exp: 'https://alltheblocks.net/maize/address/' },
+  flax: { exp: 'https://alltheblocks.net/flax/address/' },
+  aedge: { exp: 'https://alltheblocks.net/aedge/address/' },
+
+  apple: { exp: 'https://alltheblocks.net/apple/address/' },
+  wheat: { exp: 'https://alltheblocks.net/wheat/address/' },
+  dogechia: { exp: 'https://alltheblocks.net/dogechia/address/' },
+  tad: { exp: 'https://alltheblocks.net/tad/address/' },
+  taco: { exp: 'https://alltheblocks.net/taco/address/' },
+  socks: { exp: 'https://alltheblocks.net/socks/address/' },
+  mogua: { exp: 'https://alltheblocks.net/mogua/address/' },
+  mint: { exp: 'https://alltheblocks.net/mint/address/' },
 };
 
 module.exports = chainConfigs;
