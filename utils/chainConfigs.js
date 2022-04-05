2ae1ab2440445beaa2a8106319953f0fad36ca7c
raingggg <raingggg@nomail.com>
2022-04-05 13:28:44 +0800

    1.7.0

--- utils/chainConfigs.js
+++ utils/chainConfigs.js
@@ -166,6 +166,7 @@ const getMojoDivider = (blockchain) => {
   if (['shibgreen', 'littlelambocoin'].includes(blockchain)) return 1000;
   else if (['cryptodoge'].includes(blockchain)) return 1000000;
   else if (['ethgreen', 'staicoin', 'melon', 'rose'].includes(blockchain)) return 1000000000;
+  else if (['chives'].includes(blockchain)) return 100000000;
 
   return 1000000000000;
 };
